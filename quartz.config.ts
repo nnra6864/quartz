53cd6f01c071a6279b2be4559254c843ded723d3
nnra6864 <nnra2210@gmail.com>
2026-04-30 13:34:29 +0200

    changed domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "NnNotes",
+    pageTitle: "NnVault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "notes.nnstdios.xyz",
+    baseUrl: "vault.nnstdios.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
